ee60734a6517fede3a5daf189de181e60093d562
Jesus Alonso <a01793554@tec.mx>
2022-09-20 21:23:17 -0500

    Actividad Semana 1 (Avance curso Python)

--- hello.py
+++ hello.py
@@ -1,1 +1,35 @@
-print("prueba")
+# impresión de prueba
+print("Bienvenido a las pruebas de:\nJESUS ALONSO")
+
+# enteros
+var = 1
+print("Valor Entero: ", var)
+print(type(var))
+
+# float
+var = 1.67
+print("Valor Float: ", var)
+print(type(var))
+
+# booleano
+var = True
+print("Valor Booleano: ", var)
+print(type(var))
+
+# variables -> multiplicación prioridad
+var = 5 + 5 + 5 * 2
+print("Prioridad MULT [5 + 5 + 5 * 2] = ", var)
+
+# Encontrando los primeros 4 caracteres
+var = "abcdefghijkl"
+print("En el string", var, "los primeros 4 caracteres son: ", var[0:4])
+print("Cadena original min:", var, "cadena convertida a may UPPER()", var.upper())
+
+
+
+
+
+
+
+
+
